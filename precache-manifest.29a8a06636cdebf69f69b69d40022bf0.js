5188082b02cef9ee92cb46a80f1a1bfd26b55844
krislm <mcabe@me.com>
2020-08-27 00:37:58 +0200

    Updates

---
+++ precache-manifest.29a8a06636cdebf69f69b69d40022bf0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc71dd72b14c3ed0f6fee1b50b1d25a6",
+    "url": "/aju-challenge/index.html"
+  },
+  {
+    "revision": "78a1a7c441b9b72c117e",
+    "url": "/aju-challenge/static/css/main.d983002e.chunk.css"
+  },
+  {
+    "revision": "7d5d03e6d4d844b56214",
+    "url": "/aju-challenge/static/js/2.8d1f5d5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aju-challenge/static/js/2.8d1f5d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a1a7c441b9b72c117e",
+    "url": "/aju-challenge/static/js/main.4fe8bd26.chunk.js"
+  },
+  {
+    "revision": "42fa8392f9e31067a009",
+    "url": "/aju-challenge/static/js/runtime-main.76cb08b7.js"
+  },
+  {
+    "revision": "4a5372076c2dd5d64091bce2eb0a5edc",
+    "url": "/aju-challenge/static/media/Greasy.4a537207.png"
+  },
+  {
+    "revision": "780add0e4826c6ba83718411aec3ba40",
+    "url": "/aju-challenge/static/media/bmw-r80-bobber.780add0e.jpg"
+  },
+  {
+    "revision": "0989e5f42830af17466643f85050a429",
+    "url": "/aju-challenge/static/media/doodle_arrow.0989e5f4.svg"
+  },
+  {
+    "revision": "77e98af920c2b1e583f5c85a60c517b8",
+    "url": "/aju-challenge/static/media/logo.77e98af9.svg"
+  },
+  {
+    "revision": "90a9804dd4213457238d41252f2da251",
+    "url": "/aju-challenge/static/media/media_pipeburn.90a9804d.png"
+  }
+]);
